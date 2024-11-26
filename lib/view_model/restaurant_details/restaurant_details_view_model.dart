9f93d71aaeffde87e53265982e309bbf6a603c2b
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-11-26 21:33:49 +0500

    restaurant details view model

---
+++ lib/view_model/restaurant_details/restaurant_details_view_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../data/api_response.dart';
+import '../../model/model.dart';
+import '../../repository/repository.dart';
+
+class RestaurantDetailsViewModel with ChangeNotifier {
+  RestaurantDetailsRepository restaurantDetailsRepository;
+
+  RestaurantDetailsViewModel({required this.restaurantDetailsRepository});
+
+  ApiResponse<AllRestaurantsModel> _responseState = ApiResponse.loading();
+
+  ApiResponse<AllRestaurantsModel> get responseState => _responseState;
+
+  Future<void> getRestaurantDetails(int restaurantId) async {
+    _responseState = ApiResponse.loading();
+    notifyListeners();
+    try {
+      final response = await restaurantDetailsRepository.getRestaurantDetails(restaurantId);
+      _responseState = ApiResponse.loaded(response);
+    } catch (e) {
+      _responseState = ApiResponse.error(e.toString());
+    }
+    notifyListeners();
+  }
+}
--- lib/view_model/view_model.dart
+++ lib/view_model/view_model.dart
@@ -1,1 +1,2 @@
 export 'home/home_view_model.dart';
+export 'restaurant_details/restaurant_details_view_model.dart';
