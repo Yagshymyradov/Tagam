2115b36aacad6fba5e64e86e874a4330c7d8ea9e
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-12-01 01:40:24 +0500

    assets

---
+++ lib/configs/assets.dart
@@ -0,0 +1,11 @@
+final class Assets {
+  Assets._();
+
+  static const String _icons = 'assets/icons';
+  static const String _images = 'assets/images';
+  static const String ballyKoke = '$_icons/bally-koke.svg';
+  static const String checkCircle = '$_icons/check-circle.svg';
+  static const String foodGroup = '$_images/food-group.png';
+  static const String desertGroup = '$_images/desert-group.png';
+  static const String someFoods = '$_images/some-foods.png';
+}
