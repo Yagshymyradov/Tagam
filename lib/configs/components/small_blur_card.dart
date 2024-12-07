ce37bf1fe9bd74cf8d4b1990dc8e87b78b510f96
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-12-08 02:51:28 +0500

    small blur card

---
+++ lib/configs/components/small_blur_card.dart
@@ -0,0 +1,53 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../theme/colors.dart';
+
+class SmallBlurCard extends StatelessWidget {
+  final String icon;
+  final String title;
+
+  const SmallBlurCard({
+    super.key,
+    required this.icon,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: AppColors.white.withOpacity(0.12),
+            border: Border.all(
+              color: AppColors.white.withOpacity(0.06),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 8,
+            ),
+            child: Row(
+              children: [
+                SvgPicture.asset(icon),
+                const SizedBox(width: 4),
+                Text(
+                  title,
+                  style: textTheme.labelMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
